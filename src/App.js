daa9906b1a7e5d138e27934a227f16f959fd8702
lucas <lucas.kr1996@gmail.com>
2016-07-30 10:27:56 -0300

    Adicionando weather-icons na aplicação

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
+const INITIAL_CITY = 'blumenau';
+
 export default class App extends Component {
 
   constructor(props) {
-    super(props);
+    super(props); 
   }
 
   render() { }
